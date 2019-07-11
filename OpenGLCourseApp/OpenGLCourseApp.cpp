5fdcc95a2321710a559ccdd3ba1faa89d2ddbdba
Michael Schleckser <MSchleckser@Gmail.com>
2019-07-10 21:26:39 -0500

    Initial Commit

--- OpenGLCourseApp/OpenGLCourseApp.cpp
+++ OpenGLCourseApp/OpenGLCourseApp.cpp
@@ -8,6 +8,8 @@ const GLint HEIGHT = 600;
 
 GLuint VAO, VBO, shader;
 
+
+
 void createTriangle() 
 {
 	GLfloat verticies[] = {
@@ -24,7 +26,11 @@ void createTriangle()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
 
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
 }
 
 int main()
