fbd3b045f154bacf9ed8c313775fba5665365008
Michael Schleckser <MSchleckser@Gmail.com>
2019-07-10 21:24:12 -0500

    Add project files.

---
+++ OpenGLCourseApp/OpenGLCourseApp.cpp
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800;
+const GLint HEIGHT = 600;
+
+GLuint VAO, VBO, shader;
+
+void createTriangle() 
+{
+	GLfloat verticies[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f
+	};
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+}
+
+int main()
+{
+	//Initialize GLFW
+	if (!glfwInit())
+	{
+		printf("GLFW Initialization failed.");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup GLFW window properties
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// Core profile = No Backwards Compatibility
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//Allow forward compatiblity
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+
+	if (!mainWindow)
+	{
+		printf("GLFW window creation failed!");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Get Buffer Size Information
+	int bufferWidth, bufferHeight;
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	// Set context for GLEW to use.
+	glfwMakeContextCurrent(mainWindow);
+
+	//Allow modern extension features.
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		printf("GLEW intialization failed.");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	//Setup viewport size.
+	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	// Loop until window closes.
+	while (!glfwWindowShouldClose(mainWindow))
+	{
+		// Get and handle user input events
+		glfwPollEvents();
+
+		// Clear Window
+		glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(mainWindow);
+	}
+
+	return 0;
+}
